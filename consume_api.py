7cf043339ee3ae48668225a85d86f9e0657e91c5
vivy-c <cahyanivivy@gmail.com>
2024-01-05 22:25:29 +0700

    [IMP] try make api get to stackoverflow api

---
+++ consume_api.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+#https://api.stackexchange.com/
+response = requests.get(
+    'https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow'
+)
+
+print(response.json())
